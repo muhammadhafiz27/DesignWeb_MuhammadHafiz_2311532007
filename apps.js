aab397dbda15d83f3b449bc403170652e7ae7da0
muhammadhafiz27 <muhammadhafizjaya321@gmail.com>
2024-10-03 22:41:08 +0700

    commit ketiga

--- apps.js
+++ apps.js
@@ -1,10 +1,13 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js').then(registration => {
-            console.log('Service Worker registered: ', registration);
+        navigator.serviceWorker.register('./service-worker.js') // Ganti '/' dengan './' jika file ada di root.
+        .then(registration => {
+            console.log('Service Worker registered with scope:', registration.scope);
         })
         .catch(error => {
             console.error('Service Worker registration failed:', error);
         });
     });
+} else {
+    console.warn('Service Workers are not supported in this browser.');
 }
